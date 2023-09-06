62fd10bc90c4e528a871233eefd22416b668cf82
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-09-06 17:24:26 -0400

    Create hello world app

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin")
+        app.quit();
+});
+
+function createWindow() {
+    const window = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+    window.loadFile(path.join(__dirname, "index.html"));
+}
